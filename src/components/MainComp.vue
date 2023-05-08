b1c5992395fed3690c8fa61e944afba1d486ebd9
Elisabetta01 <scagione.elisabetta@gmail.com>
2023-05-08 23:26:36 +0200

    arrayCarte.length

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -18,9 +18,16 @@
 </script>
 
 <template>
+
      <div id="main" class="p-4">
-          <div class="bg-white p-4 m-4 d-flex flex-wrap justify-content-between">
-               <SingolaCarta v-for="(element, index) in store.arrayCarte" :key="index" :infoCarta="element"/>
+          <div class="bg-white p-4 m-4 ">
+               <div>
+                    <h4 class="mb-3">Found {{ store.arrayCarte.length }} cards</h4>
+               </div>
+               <div class="d-flex flex-wrap justify-content-between">
+                    <SingolaCarta v-for="(element, index) in store.arrayCarte" :key="index" :infoCarta="element"/>
+
+               </div>
           </div>
      </div>
 </template>
